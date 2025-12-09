436088a48e361b41d7519b6c453d08d517ffbf52
ZetaDaemon <zetademon@icloud.com>
2025-12-09 20:32:00 +1100

    update firing fix

--- firing_fix/__init__.py
+++ firing_fix/__init__.py
@@ -1,12 +1,29 @@
-from mods_base import build_mod, hook
+from mods_base import build_mod, hook, BoolOption
 from unrealsdk.unreal import UObject, BoundFunction, WrappedStruct
 from unrealsdk.hooks import Block
-from unrealsdk import find_object
+from unrealsdk import find_object, find_class
 from typing import Any
+
+BUTTON_STATE_Pressed = 0
+BUTTON_STATE_Held = 1
+
+
+reload_fix_option = BoolOption(
+    "Fix Zero Ammo",
+    True,
+    description="Fixes weapons getting suck at 0 ammo and being unable to fire.",
+)
+swap_fix_option = BoolOption(
+    "Fix Swap Start Fire",
+    True,
+    description="Fixes weapons not automatically starting to fire if you swap while holding fire.",
+)
 
 
 @hook("WillowGame.WillowWeapon:Active.BeginFire")
-def active_begin_fire(obj: UObject, args: WrappedStruct, *_: Any) -> None:
+def begin_fire(obj: UObject, args: WrappedStruct, *_: Any) -> None:
+    if not reload_fix_option.value:
+        return
     super_func = BoundFunction(
         find_object("Function", "Engine.Weapon.Active.BeginFire"), obj
     )
@@ -27,6 +44,24 @@ def active_begin_fire(obj: UObject, args: WrappedStruct, *_: Any) -> None:
     ):
         super_func(args.FireModeNum)
     return Block
+
+
+@hook("WillowGame.WillowWeapon:WeaponEquipping.WeaponEquipped", immediately_enable=True)
+def weapon_equipped(obj: UObject, args: WrappedStruct, *_: Any) -> None:
+    if not swap_fix_option.value:
+        return
+    if not obj.Instigator.Class._inherits(find_class("WillowPlayerPawn")):
+        return
+    player_input = obj.Instigator.Controller.PlayerInput
+    key = player_input.GetKeyForAction("Fire")
+    for button in player_input.Buttons:
+        if button.ButtonName == key and button.State in [
+            BUTTON_STATE_Held,
+            BUTTON_STATE_Pressed,
+        ]:
+            obj.SetPendingFire(0)
+            break
+    return
 
 
 build_mod()
