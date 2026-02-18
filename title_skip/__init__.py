61a5a6bcc05eb062611aaa05bdcd2f883533718c
ZetaDaemon <zetademon@icloud.com>
2026-02-18 23:59:41 +1100

    fix title skip

--- title_skip/__init__.py
+++ title_skip/__init__.py
@@ -1,15 +1,15 @@
 from typing import Any
 
-import unrealsdk
-from mods_base import hook, register_mod
+from mods_base import build_mod, hook
+from unrealsdk.unreal import UObject
 
 STARTUP_NotStarted = 0
 
 
 @hook("WillowGame.WillowGFxMoviePressStart:extBeginWait")
-def begin_wait(obj: unrealsdk.UObject, *_: Any) -> bool:
+def begin_wait(obj: UObject, *_: Any) -> bool:
     if obj.CurrentStartupStep == STARTUP_NotStarted:
         obj.CustomPlay("out")
 
 
-register_mod()
+build_mod()
