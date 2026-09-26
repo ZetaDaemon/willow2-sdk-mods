edd094bce317f2aa7406a14d7162d6fd5e1ab166
ZetaDaemon <zetademon@icloud.com>
2026-09-26 17:20:39 +1000

    update ue_timers

--- ue_timers/__init__.py
+++ ue_timers/__init__.py
@@ -3,10 +3,8 @@ from dataclasses import dataclass, field
 from typing import Any
 
 import unrealsdk
-from unrealsdk.hooks import Type
-from unrealsdk.unreal import BoundFunction, UObject, WeakPointer, WrappedStruct
-
 from mods_base import Library, bind_all_hooks, build_mod, get_pc, hook
+from unrealsdk.unreal import BoundFunction, UObject, WeakPointer, WrappedStruct
 
 __all__: tuple[str, ...] = ("UnrealTimer",)
 
@@ -15,33 +13,27 @@ __all__: tuple[str, ...] = ("UnrealTimer",)
 class UnrealTimer:
     """A Timer object that takes advantage of the unreal timers system.
 
-    Can be used as either its own object or as a decorator for the on_tick function.
+    Can be used as either its own object or as a decorator for the on_finish function.
     Timers only run while in game and not paused, so opening the menu in singleplayer
     will stop the timer. Timers will also automatically resume if left running on save quit
     or on map transition.
     The timer completely restarts on map transition, long timers may fail due to this.
 
     Args:
-        on_tick: The callback that is triggered when the timer is up.
+        on_finish: The callback that is triggered when the timer is up.
 
     """
 
-    on_finish: Callable[[], None] = field(init=True)
-    _timer_actor: WeakPointer | None = field(init=False, default_factory=WeakPointer)
-    duration: float = field(init=False, default=0)
-    loop: bool = field(init=False, default=False)
+    on_finish: Callable[..., None] = field(init=True)
+
+    _timer_actor: WeakPointer = field(init=False, default_factory=WeakPointer)
+    _kwargs: dict[str, Any] = field(init=False, default_factory=dict)
 
     def __post_init__(self) -> None:  # noqa: D105
         bind_all_hooks(self, f"{id(self)}:{id(self.on_finish)}")
 
     @hook("Engine.Actor:Timer")
-    def _timer_finish(
-        self,
-        obj: UObject,
-        _2: WrappedStruct,
-        _3: Any,
-        _4: BoundFunction,
-    ) -> None:
+    def _timer_finish(self, obj: UObject, _2: WrappedStruct, _3: Any, _4: BoundFunction) -> None:
         if obj != self._timer_actor():
             return
         timer_data = self._get_timer_data()
@@ -49,19 +41,7 @@ class UnrealTimer:
             return
         if not timer_data.bLoop:
             self.stop()
-        self.on_finish()
-
-    @hook("WillowGame.WillowGameInfo:PostCommitMapChange", Type.POST)
-    def _post_commit_map_change(
-        self,
-        _1: UObject,
-        _2: WrappedStruct,
-        _3: Any,
-        _4: BoundFunction,
-    ) -> None:
-        if self.duration == 0 and self.loop is False:
-            return
-        self.start(self.duration, self.loop)
+        self.on_finish(**self._kwargs)
 
     def _get_timer_actor(self) -> UObject:
         actor: UObject | None = self._timer_actor()
@@ -72,8 +52,7 @@ class UnrealTimer:
         return actor
 
     def _get_timer_data(self) -> WrappedStruct | None:
-        timer_actor = self._timer_actor()
-        if self._timer_actor() is None:
+        if (timer_actor := self._timer_actor()) is None:
             return None
         for timer in timer_actor.Timers:
             if timer.FuncName == "Timer":
@@ -82,13 +61,11 @@ class UnrealTimer:
 
     def _enable_hooks(self) -> None:
         self._timer_finish.enable()
-        self._post_commit_map_change.enable()
 
     def _disable_hooks(self) -> None:
         self._timer_finish.disable()
-        self._post_commit_map_change.disable()
 
-    def start(self, duration: float, loop: bool) -> None:
+    def start(self, duration: float, loop: bool, **kwargs: Any) -> None:
         """Start the timer.
 
         Args:
@@ -96,10 +73,11 @@ class UnrealTimer:
             loop: If true the timer will restart automatically with the same values.
             start_time: The starting time for the timer, used to make the first tick of
                         the timer a different legnth, the value is limited to length.
+            kwargs: Optional kwargs to be passed into the callback
 
         """
         timer_actor = self._get_timer_actor()
-        if self._get_timer_data() is not None:
+        if self.is_running():
             msg = "Cannot start a timer that is running."
             raise RuntimeError(msg)
         # FuncName="Timer" is done so that Engine.Actor:Timer is called on finish.
@@ -113,14 +91,11 @@ class UnrealTimer:
         )
         timer_actor.Timers = [timer_data]
         self._enable_hooks()
-        self.duration = duration
-        self.loop = loop
+        self._kwargs = kwargs
 
     def stop(self) -> None:
         """Stop the timer. Must do this when finished with a timer so it can be removed properly."""
         self._disable_hooks()
-        self.duration = 0
-        self.loop = False
         timer_actor = self._timer_actor()
         if timer_actor is None or self._get_timer_data() is None:
             msg = "Cannot stop a timer that is not running."
@@ -141,7 +116,7 @@ class UnrealTimer:
         if timer_data is None:
             msg = "Cannot resume a timer that is not running."
             raise RuntimeError(msg)
-            return
+
         timer_data.bPaused = False
 
     def update(self, duration: float, loop: bool) -> None:
@@ -157,8 +132,6 @@ class UnrealTimer:
             return
         timer_data.Rate = duration
         timer_data.bLoop = loop
-        self.duration = duration
-        self.loop = loop
 
     def is_running(self) -> bool:
         """Check if the timer has been started."""
--- ue_timers/readme.md
+++ ue_timers/readme.md
@@ -1,4 +1,5 @@
 # UE Timers
 
 ## Changelog
-- 1.0: Initial release.
+- 2: Removed post map change callback, seemed to be redundant. Cleaned up code. Added kwargs for the caallback.
+- 1: Initial release.
