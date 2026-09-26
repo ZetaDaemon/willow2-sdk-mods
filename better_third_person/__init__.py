9331ea3ef0cb0d57a61405ad08d533b8d5eb7365
ZetaDaemon <zetademon@icloud.com>
2026-09-26 17:33:39 +1000

    fix better third person

--- better_third_person/__init__.py
+++ better_third_person/__init__.py
@@ -265,7 +265,7 @@ def set_zoom_state(
         case EZoomState.ZST_Zoomed:
             if controller.bBehindView:
                 if aim_mode.value == AimZoomMode.SCOPE or should_stop_third_person:
-                    stop_third_person(owner.Controller)
+                    controller.SetBehindView(False)
                     remove_fov_modifier(weapon)
                     weapon.ZoomedFOV = weapon.ZoomedEndFOV
                     should_stop_third_person = False
